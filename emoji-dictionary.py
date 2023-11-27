7f582a551e82926d74214ccfd116b5604933b5ae
Ty Rickers <ty.rickers@gmail.com>
2023-11-27 12:20:01 -0700

    updated the parser function to get json format that works with unity. also added removal of duplicates. Next step is to add more detailed codes

--- emoji-dictionary.py
+++ emoji-dictionary.py
@@ -22,58 +22,61 @@ emoticons_dict = {
 }
 
 def parse_emoji_data(file_path):
-    emoji_dict = {}
+    emoji_data = {"groups": []}
     current_group = None
     current_subgroup = None
     current_category = None
+    processed_codes = set()  # Set to keep track of processed codes
 
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
             if line.startswith('# group:'):
-                current_group = line.split(': ')[1].strip()
-                emoji_dict[current_group] = {
-                    "interjection": interjections.get(current_group, "!"),
-                    "subgroups": {}
+                current_group = {
+                    "name": line.split(': ')[1].strip(),
+                    "interjection": interjections.get(line.split(': ')[1].strip(), "!"),
+                    "subgroups": []
                 }
+                emoji_data["groups"].append(current_group)
             elif line.startswith('# subgroup:'):
-                current_subgroup = line.split(': ')[1].strip()
-                if '-' in current_subgroup:
-                    current_subgroup, current_category = current_subgroup.split('-', 1)
-                else:
-                    current_category = 'general'
-                emoji_dict[current_group]["subgroups"].setdefault(current_subgroup, {
-                    "interjection": interjections.get(current_subgroup, "!"),
-                    "categories": {}
-                })
-                emoji_dict[current_group]["subgroups"][current_subgroup]["categories"].setdefault(current_category, {
-                    "interjection": interjections.get(current_category, "!"),
-                    "emojis": []
-                })
+                current_subgroup = {
+                    "name": line.split(': ')[1].strip(),
+                    "categories": []
+                }
+                current_group["subgroups"].append(current_subgroup)
             elif not line.startswith('#') and line.strip():
                 parts = line.split(';')
                 emoji_code = parts[0].strip().split()[0]
-                emoji_and_description = parts[-1].strip().split('# ')[1]
-                description = re.sub(r' E\d+\.\d+', '', emoji_and_description)
+                # Skip if emoji code has already been processed
+                if emoji_code in processed_codes:
+                    continue
+                processed_codes.add(emoji_code)
+                emoji_char_and_description = parts[-1].strip().split('# ')[1]
+                description = re.sub(r' E\d+\.\d+', '', emoji_char_and_description)
                 emoji_char, _, description_text = description.partition(' ')
                 emoticons = emoticons_dict.get(emoji_char, [])
-                emoji_dict[current_group]["subgroups"][current_subgroup]["categories"][current_category]["emojis"].append([emoji_code, emoji_char, description_text, emoticons])
+                emoji_info = {
+                    "code": emoji_code,
+                    "character": emoji_char,
+                    "description": description_text.strip(),
+                    "emoticons": emoticons
+                }
+                if '-' in current_subgroup["name"]:
+                    current_subgroup_name, current_category_name = current_subgroup["name"].split('-', 1)
+                else:
+                    current_subgroup_name = current_subgroup["name"]
+                    current_category_name = 'general'
+                # Find or create the category
+                category = next((cat for cat in current_subgroup["categories"] if cat["name"] == current_category_name), None)
+                if not category:
+                    category = {"name": current_category_name, "interjection": interjections.get(current_category_name, "!"), "emojis": []}
+                    current_subgroup["categories"].append(category)
+                category["emojis"].append(emoji_info)
 
-    return emoji_dict
+    return emoji_data
 
 # Usage
 if __name__ == "__main__":
     emoji_data = parse_emoji_data('emoji-test.txt')
-
-    # Writing to a text file
-    with open('parsed_emoji_data.txt', 'w', encoding='utf-8') as file:
-        for group, group_data in emoji_data.items():
-            file.write(f"Group: {group} - {group_data['interjection']}\n")
-            for subgroup, subgroup_data in group_data["subgroups"].items():
-                file.write(f"  Subgroup: {subgroup} - {subgroup_data['interjection']}\n")
-                for category, category_data in subgroup_data["categories"].items():
-                    file.write(f"    Category: {category} - {category_data['interjection']}\n")
-                    for emoji_info in category_data["emojis"]:
-                        file.write(f"      {emoji_info[1]} ({emoji_info[0]}): {emoji_info[2]}, Emoticons: {', '.join(emoji_info[3])}\n")
 
     # Writing to JSON file
     with open('parsed_emoji_data.json', 'w', encoding='utf-8') as file:
